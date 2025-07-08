830fca357e83249d304faec5c420b50a3c80687f
Thierry-emmanuel <emmanueltsafack2005@gmail.com>
2025-07-08 10:46:36 +0100

    Add files via upload

--- app1/admin.py
+++ app1/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import Feature
 from .models import Estate
 from .models import QuickOrder
+from .models import Review
 from .models import ContactRequest
+
 # Register your models here.
 
 admin.site.register(Feature)
 admin.site.register(Estate)
 admin.site.register(QuickOrder)
 admin.site.register(ContactRequest)
+admin.site.register(Review)
