2281e0d0038b53233caebe2308a198abc7acfea6
abba <adjiabba10@gmail.com>
2025-07-20 19:58:57 +0100

    allowedhostmodification

--- project1/settings.py
+++ project1/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+ALLOWED_HOSTS = ['127.0.0.1','eyang-estate.onrender.com', 'localhost', ]
 
 
 
