4c39e30f6e5801ab15365fb4240be32282d0dc2f
Abba-Max <adjiabba10@gmail.com>
2026-04-29 00:45:32 +0200

    Fix ASGI initialization order

--- backend/project/asgi.py
+++ backend/project/asgi.py
@@ -9,15 +9,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from apps.estate_app.middleware import JWTAuthMiddleware
 from apps.estate_app.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": JWTAuthMiddleware(
         URLRouter(
             websocket_urlpatterns
